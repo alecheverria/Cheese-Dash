a95e3aec49aacd10446ac85a23b8d9182899a4f0
lin00 <lchen00000@gmail.com>
2015-07-27 15:05:19 -0700

    update player image and floor

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 var game;
+var cursors;
 var mainMenu = {
     preload : function() {},
     create : function() {},
@@ -7,45 +8,75 @@ var mainMenu = {
 var mainState = {
     //loads all the assets
     preload : function() {
-        game.load.image('player', 'images/tempPlatform.png');
+        game.load.image('player', 'images/mouse.png');
+        game.load.image('bg', 'images/bkg.png');
+        game.load.image('floor', 'images/floor.png');
     },
     
     //adds it to the game
     create : function() {
         game.physics.startSystem(Phaser.Physics.ARCADE);
         
+        //backgrounds and cheating floor
+        this.bg = game.add.sprite(0,0, 'bg');
+        this.floor = game.add.sprite (0 , game.world.height - 72, 'floor');
+        game.physics.arcade.enable(this.floor);
+        this.floor.body.immovable = true;
+                
         //platforms
-        this.player = game.add.sprite(100, 500 ,'player');
-        this.player.scale.x = 0.3; 
-        this.player.scale.y = 0.5;
+        this.player = game.add.sprite(100, 300 ,'player');
+        this.player.scale.setTo(0.3,0.3);
         game.physics.arcade.enable(this.player);
-        this.player.body.colliderWorldBounds = true;
+        this.player.body.gravity.y = 800;
+        this.player.body.collideWorldBounds = true;
         
         //controls
-        var up = this.game.input.keyboard.addKey(Phaser.Keyboard.UP);
-            up.onDown.add(this.jump,this);
+        cursors = game.input.keyboard.createCursorKeys();
+        this.consecutiveJumps = 2;
+        
+        
         
     },
     update: function() {
-        if (this.player.body.y >= game.world.height- this.player.height ) {
-            this.player.body.velocity.y = 0;
-            this.player.body.gravity.y = 0;
-        } else {
-            this.player.body.gravity.y = 800;
+        this.player.body.velocity.x = 0;
+        
+        game.physics.arcade.collide(this.player, this.floor);
+        game
+        
+        if (cursors.left.isDown){
+            //  Move to the left
+            console.log("left pressed");
+            this.player.body.velocity.x = -150;
 
         }
-    },
-    
-    jump: function() {
-        var consecutiveJumps = 2;
-        if (consecutiveJumps > 0) {
-            this.player.body.velocity.y = -300;
-            consecutiveJumps --;
-        } else if (consecutiveJumps === 0) {
-            
-        }
+        else if (cursors.right.isDown){
+            //  Move to the right
+            this.player.body.velocity.x = 150;
+
+        } 
+        if (cursors.up.isDown)
+            jump();
         
-    }
+        
+        
+        
+        
+
+   },
+       jump: function() {
+            if (this.consecutiveJumps > 0 && this.player.body.touching.down) {  
+                this.player.body.velocity.y = -350;
+                this.consecutiveJumps --;
+                if (this.consecutiveJumps = 1) {
+                    this.player.body.velocity.y = -350;
+                    this.consecutiveJumps = 0;
+                }
+                
+
+            }
+           this.consecutiveJumps = 2;
+        }
+
 };
 
 
