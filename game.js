59151f279fa9a4f0f245bd322526331cfb77140a
dpweb <dpweb@g-4.bavc-int.org>
2015-07-30 18:01:57 -0700

    Added win and lose condition

--- game.js
+++ game.js
@@ -20,6 +20,9 @@ var mainState = {
 		game.load.image('cheese', 'images/sexy-cheese2.png');
 		game.load.image('ground', 'images/bricks.png');
 		game.load.spritesheet('cats', 'images/cats2.png', 440, 345);
+		game.load.image('win', 'images/winc.png');
+		game.load.image('lose', 'images/game-over.png');
+
     },
     
 	
@@ -29,12 +32,11 @@ var mainState = {
         game.physics.startSystem(Phaser.Physics.ARCADE);
         
         //backgrounds and cheating floor
-        this.bg = game.add.sprite(0,0, 'bg');
+        this.bg = game.add.sprite(0,-200, 'bg');
         this.floor = game.add.sprite (0 , game.world.height - 72, 'floor');
         game.physics.arcade.enable(this.floor);
         this.floor.body.immovable = true;
 		this.floor.enableBody = true;
-		
 		//  The platforms group contains the ground and the 2 ledges we can jump on
     platforms = game.add.group();
 
@@ -42,11 +44,7 @@ var mainState = {
     platforms.enableBody = true;
 		
 		//  Now let's create two ledges
-    var ledge = platforms.create(300, 600, 'ground');
-
-    ledge.body.immovable = true;
-
-    ledge = platforms.create(-150, 270, 'ground');
+    var ledge = platforms.create(-150, 270, 'ground');
 
     ledge.body.immovable = true;
 
@@ -83,14 +81,16 @@ var mainState = {
     this.enemy.body.collideWorldBounds = true;
 
     //  Our two animations, walking left and right.
-    this.enemy.animations.add('left', [0, 1], 4, true);
-    this.enemy.animations.add('right', [3, 4], 4, true);
+    this.enemy.animations.add('left', [0, 1], 3, true);
+	
+    this.enemy.animations.add('right', [2, 3], 3, true);
 		
 	// Animations for Enemy
 	// Cat Animations
-	this.enemy.animations.add('walk');
-	this.enemy.animations.play('walk', 4, true);
-    game.add.tween(this.enemy).to({ x:356}, 10000, Phaser.Easing.Linear.None, true);
+//	this.enemy.animations.add('walk');
+//	this.enemy.animations.play('walk', 3, true);
+    game.add.tween(this.enemy).to({ x:156}, 10000, Phaser.Easing.Linear.None, true);
+		this.enemy.animations.play('left');
 
 	//controls
 	cursors = game.input.keyboard.createCursorKeys();
@@ -116,15 +116,17 @@ var mainState = {
 	cheese.body.bounce.y = 0.3 + Math.random() * 0.2;
 	}
     // World Bounds
-    game.world.setBounds(0, 0, 2400, 600);
+    game.world.setBounds(0, -200, 2400, 800);
     
 		
     //  The score
     scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-	
+	scoreText.fixedToCamera = true;
+		
     
 		// Game Camera
 	    game.camera.follow(this.player);
+        
 		
     },
     update: function() {
@@ -140,6 +142,23 @@ var mainState = {
 
 		}
 		
+
+		function AttackCat (player, enemy) {
+
+			// Removes the enemy from the screen
+			
+			
+			if (player.position.y <= enemy.position.y) {
+				enemy.kill();
+			}
+			else {
+				player.kill();
+            this.lose = game.add.sprite(game.camera.x,game.camera.y, 'lose');
+            game.camera.unfollow(this.player);	
+			}
+			
+		}
+
 		this.player.body.velocity.x = 0;
         
         game.physics.arcade.collide(this.player, this.floor);
@@ -150,7 +169,7 @@ var mainState = {
 		
 		//  Checks to see if the player overlaps with any of the cheeses, if he does call the collectCheese function
 		game.physics.arcade.overlap(this.player, cheeses, collectCheese, null, this);
- 
+        game.physics.arcade.overlap(this.player, this.enemy, AttackCat, null, this);
         
         if (cursors.left.isDown){
             //  Move to the left
@@ -164,33 +183,42 @@ var mainState = {
 
         } 
         if (cursors.up.isDown && this.player.body.touching.down){
-            this.player.body.velocity.y = -550;
+            this.player.body.velocity.y = -650;
         
         }
 		
-		// Animation for Enemy
-		this.enemy.x -= .50;
+//		// Animation for Enemy
+//		this.enemy.x -= .50;
 
 //		if (this.enemy.x < -this.enemy.width)
 //		{
 //		this.enemy.x = game.world.width;
 //		}
-		
+		// Left
 		if (this.enemy.position.x <= 356) {
-		game.add.tween(this.enemy).to({ x:1118}, 10000, Phaser.Easing.Linear.None, true);		
+		game.add.tween(this.enemy).to({x:1118}, 10000, Phaser.Easing.Linear.None, true);
+			this.enemy.animations.stop();
+			this.enemy.animations.play('right');
+			console.log(this.enemy.animations);
 		}
-		// use left
-		if (this.enemy.position.x >= 1118) {
+		else if (this.enemy.position.x >= 1118) {
 		game.add.tween(this.enemy).to({ x:356}, 10000, Phaser.Easing.Linear.None, true);
+			this.enemy.animations.play('left');
 		}
-		// use right
-   },
-	render: function () {
+		
+		if (score >= 5) {			
+			this.win = game.add.sprite(game.camera.x,game.camera.y, 'win');
+            game.camera.unfollow(this.player);	
+		}
+		
+	}
+//	render: function () {
+//
+//    game.debug.cameraInfo(game.camera, 32, 32);
+//    game.debug.spriteCoords(this.player, 32, 500);
+//
+//	}
 
-    game.debug.cameraInfo(game.camera, 32, 32);
-    game.debug.spriteCoords(this.player, 32, 500);
-
-}
 };
 
 	
